80ff866bda80563312e11600840371a756e1242a
Victorino Machava <vitomachava@gmail.com>
2017-10-20 01:12:38 +0200

    Refactor code to make use of arrays and functions - Less hardcoding

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,14 +1,14 @@
-const blankLine = '  |   |  ';
+const printBoard = (board) => {
+  console.log("Current Board:");
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 
-console.log("This is what an empty board would look like:");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
 
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-
-console.log("This is what a board with a guess and a bomb on it would look like:");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+printBoard(board);
