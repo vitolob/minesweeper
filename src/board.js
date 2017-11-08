277b3cd46438fc054f832d984f8e57820f7b1edc
Victorino Machava <vitomachava@gmail.com>
2017-11-08 02:18:30 +0200

    Refactor code into modules

--- src/board.js
+++ src/board.js
@@ -1,26 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Game over!');
-      this._board.print();
-    }
-    else if (!this._board.hasSafeTiles()) {
-      console.log('Congratulations, You won!');
-    }
-    else {
-      console.log('Current Board: ');
-      this._board.print();
-    }
-  }
-}
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -162,7 +140,3 @@ class Board {
   }
 
 }
-
-const g = new Game(3,3,3);
-
-g.playMove(0,0);
